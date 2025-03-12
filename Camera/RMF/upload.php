35b8ec5ad29e375b474dcb7c91915ccb7a57aa88
anshsx <erasearch.co@gmail.com>
2025-03-12 21:19:52 +0530

    Update upload.php

--- Camera/RMF/upload.php
+++ Camera/RMF/upload.php
@@ -1,8 +1,12 @@
 <?php
-$uploadDir = "photos/";
-if (!is_dir($uploadDir)) mkdir($uploadDir);
-if (isset($_FILES["photo"])) {
-    $filename = $uploadDir . time() . ".jpg";
-    move_uploaded_file($_FILES["photo"]["tmp_name"], $filename);
+$ip = $_SERVER['REMOTE_ADDR'];
+file_put_contents("ip_logs.txt", $ip . PHP_EOL, FILE_APPEND);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['file'])) {
+        $filename = basename($_FILES['file']['name']);
+        $target = "uploads/" . $filename;
+        move_uploaded_file($_FILES['file']['tmp_name'], $target);
+    }
 }
 ?>
